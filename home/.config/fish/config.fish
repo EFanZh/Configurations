6dca49f720784f8a27fe446bac966e062d85fdfa
EFanZh <efanzh@gmail.com>
2023-12-11 11:31:13 +0800

    Update config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,2 @@
 set fish_prompt_pwd_dir_length 0
-
-for x in $HOME/.cargo/bin $HOME/bin
-    if [ -d $x ]
-        set PATH (string match -v $x $PATH)
-        set PATH $x $PATH
-    end
-end
+fish_add_path $HOME/bin $HOME/.cargo/bin
